f960c5d870cf054b1768b845e688f5dae91b69c9
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-03-06 11:06:16 +0300

    Add pure version of code.

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,23 +1,12 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  let spacing = document.getElementById('spacing');
-  let blur = document.getElementById('blur');
-  let color = document.getElementById('base');
-
-  spacing.addEventListener('change', onChangeSpacing);
-  blur.addEventListener('change', onChangeBlur);
-  color.addEventListener('change', onChangeColor);
+  const inputs = document.querySelectorAll('.controls input');
+  inputs.forEach(input => input.addEventListener('change', handleUpdate));
+  inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 }
 
-function onChangeSpacing(event) {
-  document.documentElement.style.setProperty('--spacing', `${event.target.value}px`);
-}
-
-function onChangeBlur(event) {
-  document.documentElement.style.setProperty('--blur', `${event.target.value}px`);
-}
-
-function onChangeColor(event) {
-  document.documentElement.style.setProperty('--base', event.target.value);
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
